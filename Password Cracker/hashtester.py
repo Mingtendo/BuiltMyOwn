9a9618f97585834c7a7e8ac81628f1c88f976bd5
Mingtendo <mingtendo@gmail.com>
2024-03-20 15:57:30 -0400

    Refactored the Python code to use a function to test passwords. Learned that you can encoding a string using .encode(encoding='encoding_type').

--- Password Cracker/hashtester.py
+++ Password Cracker/hashtester.py
@@ -1,6 +1,10 @@
 from hashlib import md5
 
-print("Result of hashing \'password\':")
-print(md5(b"password").hexdigest())
-print("Result of hashing \'The quick brown fox jumps over the lazy dog\'")
-print(md5(b"The quick brown fox jumps over the lazy dog").hexdigest())
+def testhash(word: str):
+    encoded = word.encode(encoding='ascii')
+    return md5(encoded).hexdigest()
+
+passwords = ["password", "The quick brown fox jumps over the lazy dog", ""]
+
+for pw in passwords:
+    print(pw, testhash(pw))
